c93cfeebe2527d6de8430863a68d0b55cb054ca1
aarontravass <aaron.travass@hotmail.com>
2023-07-01 16:43:38 -0400

    added phase

---
+++ src/database/database.py
@@ -0,0 +1,5 @@
+import psycopg2
+
+conn_string = "host='ep-patient-sky-333978-pooler.us-east-1.aws.neon.tech' dbname='test' user='aarontravass' password='QbGq3Jv6zhAM'"
+conn = psycopg2.connect(conn_string)
+

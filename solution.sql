c2408b47dff8849f21694db2a07323df035a46f2
aarontravass <aaron.travass@hotmail.com>
2023-07-02 02:59:03 -0400

    added misc files

--- solution.sql
+++ solution.sql
@@ -6,8 +6,10 @@ WHERE test_plan_phases.manager_user_id=3
 GROUP BY test_plan.id;
 
 -- b. Write SQL to find all the Test Plans that are still pending (i.e., have an unapproved Phase).Return only data for Test Plans. Ensure there are no duplicate rows in the results.
-SELECT test_plan.id, plan_name FROM test_plan 
-WHERE  test_plan.approved_on IS null
+SELECT test_plan.id, plan_name FROM test_plan INNER JOIN test_plan_phases
+ON test_plan.id = test_plan_phases.test_plan_id
+WHERE test_plan_phases.approved_on IS null
+GROUP BY test_plan.id;
 
 -- c. Write SQL to add an additional Phase to a particular Test Plan.
 

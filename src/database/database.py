271b480d1543e3305afdf658f4700e9d0f3b809e
aarontravass <aaron.travass@hotmail.com>
2023-07-02 01:53:42 -0400

    added files

--- src/database/database.py
+++ src/database/database.py
@@ -1,5 +1,6 @@
 import psycopg2
 
-conn_string = "host='localhost' dbname='postgres' user='admin' password='admin'"
+# conn_string = "host='localhost' dbname='postgres' user='admin' password='admin'"
+conn_string = "host='ep-patient-sky-333978-pooler.us-east-1.aws.neon.tech' dbname='test' user='aarontravass' password='QbGq3Jv6zhAM'"
 conn = psycopg2.connect(conn_string)
 
